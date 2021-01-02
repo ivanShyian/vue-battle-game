b5fb172b1a36cad5ab119ffab084db3978d1503e
vanjkes <shyian.ivan@yahoo.com>
2021-01-02 21:34:29 +0200

    Configured health-box and special-hit

--- app.js
+++ app.js
@@ -6,12 +6,31 @@ const app = Vue.createApp({
     data() {
         return {
             playerHealth: 100,
-            monsterHealth: 100
+            monsterHealth: 100,
+            currentRound: 0,
+        }
+    },
+    computed: {
+        calcMonsterHealth() {
+            return {width: this.monsterHealth + '%'}
+        },
+        calcPlayerHealth() {
+            return {width: this.playerHealth + '%'}
+        },
+        showSpecialAttack() {
+            return this.currentRound % 3 !== 0;
         }
     },
     methods: {
+        specialAttack() {
+            this.currentRound++;
+            const attackValue = getRandomValue(10, 24);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
         attackMonter () {
             // Math.random() * (max-min) + min;
+            this.currentRound++;
             const attackValue = getRandomValue(6, 15);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
