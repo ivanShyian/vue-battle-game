e8c37faf017526804142c29d1a20b74e076595db
vanjkes <shyian.ivan@yahoo.com>
2021-01-02 21:55:52 +0200

    Configured healing btn

--- app.js
+++ app.js
@@ -12,12 +12,18 @@ const app = Vue.createApp({
     },
     computed: {
         calcMonsterHealth() {
-            return {width: this.monsterHealth + '%'}
+            return {
+                width: this.monsterHealth + '%'
+            }
         },
         calcPlayerHealth() {
-            return {width: this.playerHealth + '%'}
+            return {
+                width: this.playerHealth + '%'
+            }
         },
         showSpecialAttack() {
+            // if number not deviding by 3 - true (:disable="true")
+            // if number devidibg by 3 - false (:disable="false")
             return this.currentRound % 3 !== 0;
         }
     },
@@ -28,16 +34,28 @@ const app = Vue.createApp({
             this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
-        attackMonter () {
+        attackMonter() {
             // Math.random() * (max-min) + min;
             this.currentRound++;
-            const attackValue = getRandomValue(6, 15);
+            const attackValue = getRandomValue(5, 12);
             this.monsterHealth -= attackValue;
             this.attackPlayer();
         },
         attackPlayer() {
-            const attackValue = getRandomValue(8, 19);
+            const attackValue = getRandomValue(9, 19);
             this.playerHealth -= attackValue;
+        },
+        healPlayer() {
+            this.currentRound++;
+            const healValue = getRandomValue(12, 22);
+            console.log(this.playerHealth + healValue)
+            if (this.playerHealth + healValue >= 100) {
+                this.playerHealth = 100;
+            } else {
+                this.playerHealth += healValue;
+            }
+            this.attackPlayer();
+
         }
     }
 });
